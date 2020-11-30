7f177a3503a9e466ca0fb9433e7e043866084b52
may4everL <weis9004@gmail.com>
2020-11-30 01:58:47 -0500

    change port

--- app.js
+++ app.js
@@ -164,7 +164,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Serving on port ${PORT}`);
